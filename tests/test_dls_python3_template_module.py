10f60fdfa3d3c5057c6133af90f4c15a7224ccea
Niamh Dougan <niamh.dougan@diamond.ac.uk>
2020-03-30 13:46:47 +0100

    Adding ideas for things to test

---
+++ tests/test_dls_python3_template_module.py
@@ -0,0 +1,31 @@
+import pytest
+
+# README
+
+def test_changed_README():
+    pass
+
+# Metadata in setup.cfg
+
+def test_module_name():
+    pass
+
+def test_module_description():
+    pass
+
+def test_module_url():
+    pass
+
+def test_module_author():
+    pass
+
+def test_module_author_email():
+    pass
+
+# Docs
+
+def test_docs_boilerplate_removed():
+    pass
+
+def test_docs_conf_general_info_changed():
+    pass
--- tests/test_dls_python_template_module.py
+++
@@ -1,4 +0,0 @@
-import pytest
-
-def test_removed_docs_boilerplate():
-    assert False
