89f81b500318f7360598fc8155461b1ca5a05965
Niamh Dougan <niamh.dougan@diamond.ac.uk>
2020-03-31 10:24:51 +0100

    Adding tests for setup.cfg

--- tests/test_dls_python3_template_module.py
+++ tests/test_dls_python3_template_module.py
@@ -1,26 +1,36 @@
+import configparser
 import pytest
 
-# README
 
-def test_changed_README():
-    pass
+class Setupcfg():
+    def __init__(self):
+        self.conf = configparser.ConfigParser()
+        self.conf.read('setup.cfg')
+        self.meta = self.conf['metadata']
 
-# Metadata in setup.cfg
+    def test_module_name(self): 
+        assert self.meta['name']!='dls_python3_template_module', \
+            "Did you remember to change the module name in setup.cfg?"
 
-def test_module_name():
-    pass
+    def test_module_description(self):
+        assert self.meta['description']!=\
+            'Tool for adding version from git to a python project without adding dependencies',\
+            "Please write a description for your module."
 
-def test_module_description():
-    pass
+    def test_module_url(self):
+        assert self.meta['url']!='https://github.com/dls-controls/versiongit',\
+            "Please add a URL for your module repo."
 
-def test_module_url():
-    pass
+    def test_module_author(self):
+        if self.meta['author'] in self.meta:
+            assert self.meta['author']!= 'None',\
+                "Please add an author or remove this line from setup.cfg"
 
-def test_module_author():
-    pass
+    def test_module_author_email(self):
+        if self.meta['author_email'] in self.meta:
+            assert self.meta['author_email']!= 'None',\
+                "Please add an author email or remove this line from setup.cfg"
 
-def test_module_author_email():
-    pass
 
 # Docs
 
@@ -29,3 +39,9 @@ def test_docs_boilerplate_removed():
 
 def test_docs_conf_general_info_changed():
     pass
+
+# README
+
+def test_changed_README():
+    pass
+
